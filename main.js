2963c5e0d4a23d217e4c846e5cbbee4d089fbf1b
Drew <Drew@users-MacBook-Pro-1055.local>
2015-11-01 22:21:59 -0800

    refactored version

--- main.js
+++ main.js
@@ -5,49 +5,47 @@ $(document).ready(init);
 function init () {
   $('.tower').on('click', boxClicked);
   $('.submit').on('click', createDivs)
-}
 
+}
 function createDivs(){
   var divCount = $('.numberSelect').val();
   var divCountInt=parseInt(divCount);
-  console.log(parseInt(divCount) + 1);
   for (var i=1; i< divCountInt+1; i++){
-
-    var thisDiv = $('<div class="class'+i+'" id="'+i+'">');
-    console.log(thisDiv);
-    $('.first').append(thisDiv);
+      var thisDiv = $('<div class="class'+i+'" id="'+i+'">');
+      $('.first .contentContain').prepend(thisDiv);
+      $('.numberSelect').attr('disabled',' disabled')
   }
-
-
-
 }
 
-function boxClicked(event){
-  var $this = $(this);
-  var $thisSelected = $('.selected');
-  var $childrenInThisTower = $this[0].children;
 
-  if ($this.children(':last-child').hasClass('selected')){
-      $this.children(':last-child').removeClass('selected');
+
+function boxClicked(event){
+
+
+  var $this = $(this);
+
+  var $thisSelected = $('.selected');
+  var $childrenInThisTower = $this.children('.contentContain').children('div');
+  if ($childrenInThisTower.hasClass('selected')){
+      $childrenInThisTower.removeClass('selected');
   } else{
-    if($this.siblings().children(':last-child').hasClass('selected')) {
+    if($this.siblings().find('.contentContain').children(':first-child').hasClass('selected')) {
       for(var i = 0; i < $childrenInThisTower.length; i++) {
 
         if(parseInt($childrenInThisTower[i].id) > parseInt($thisSelected[0].id)) {
           return;
         }
       }
-
       var detached = $('.selected').removeClass('selected').detach();
-      $this.append(detached);
+      $this.children('.contentContain').prepend(detached);
     } else {
-      $(this).children(':last-child').addClass('selected');
+      $(this).children('div').children(':first-child').addClass('selected');
     }
-
   }
-  if ($('.thirdTower')[0].children.length===3){
-    alert('YOUR A UNICORN BLASTER!');
-    console.log('youwin');
 
+  if ( $('.thirdTower').find('.contentContain').children('div').length===parseInt($('.numberSelect').val())){
+    alert('YOU WIN!!!');
+    location.reload();
   }
 }
+//////////////////////////////////////////////////////////////////////////////////////
